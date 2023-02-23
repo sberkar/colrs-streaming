d09895a2e46656974ef0066e862bf0651d6179db
SHUBHAM BERKAR <39162880+sberkar@users.noreply.github.com>
2023-02-23 15:25:12 +0000

    made home route non private

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to={"/app"} replace={true} />} />
          <Route path="/app">
-          <Route path=""element={isAuthenticated?<Pages.Home />:<Navigate to="/app/login" />} />
+          <Route path=""element={<Pages.Home />} />
           <Route path="login" element={isAuthenticated?<Navigate to="/app" replace />:<Pages.Login />}/>
           <Route path="create" element={isAuthenticated?<Pages.CreateUrl />:<Navigate to="/app/login" replace />} />
           <Route path="profile" element={isAuthenticated?<Pages.Profile />:<Navigate to="/app/login" replace />} />
